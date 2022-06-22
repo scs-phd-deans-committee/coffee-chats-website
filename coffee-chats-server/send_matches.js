68cce237203fa1e97eb6171e29c6952803bf5163
Shahul Alam <alam.shahul@gmail.com>
2022-06-22 18:11:20 -0400

    Files for sending match files.

--- coffee-chats-server/send_matches.js
+++ coffee-chats-server/send_matches.js
@@ -1,4 +1,18 @@
-const sgMail = require('@sendgrid/mail')
+const sgMail = require('@sendgrid/mail');
+const admin = require("./firebaseAdmin");
+
+const fs = require('fs');
+
+let matchesRef = admin.firestore.collection("responses").where("round", "==", 0);
+
+matchesRef.get().then((snapshot) => {
+  let data = snapshot.docs.map(doc => doc.data());
+
+  let jsonifiedData = JSON.stringify(data, null, 2);
+  console.log(JSON.stringify(data));
+  fs.writeFile("form_responses.txt", jsonifiedData, function(err) { if (err) { console.log(err); } });
+});
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
       to: ['hlzhou@andrew.cmu.edu', 'rveerapa@andrew.cmu.edu', 'shahula@andrew.cmu.edu'], // Change to your recipient
@@ -7,12 +21,11 @@ const msg = {
       text: 'hello',
       html: '<strong>hello</strong>',
 }
-sgMail
-  .send(msg)
-  .then(() => {
-          console.log('Email sent')
-        })
-  .catch((error) => {
-          console.error(error)
-        })
 
+sgMail.send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
